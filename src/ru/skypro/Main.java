7a9da832fd427f0807fc6e77140f70d245d4d353
Kaplin Sergey <kaplin@internet.ru>
2021-11-17 23:15:21 +0300

    первый коммит

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,13 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte byteVariable = 127;
+        short shortVariable = 128;
+        int intVariable = 9898;
+        long longVariable = 200_000L;
+        float floatVariable = 10.0F;
+        double doubleVariable = 69.69D;
+    }
+}
